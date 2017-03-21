4d524f5c59020f0b127fb091a52f72d12a0d0ed9
Vinyas N S <vinyas.n.s@gmail.com>
2017-03-22 00:43:16 +0530

    Java implementation of BinaryHeap

---
+++ Java/Algorithm/src/org/openalgorithm/InvalidOperationException.java
@@ -0,0 +1,13 @@
+package org.openalgorithm;
+
+public class InvalidOperationException extends Exception    {
+    public InvalidOperationException()  {
+        super();
+    }
+    public InvalidOperationException(String message)    {
+        super(message);
+    }
+    public InvalidOperationException(String message, Throwable cause)   {
+        super(message,cause);
+    }
+}
