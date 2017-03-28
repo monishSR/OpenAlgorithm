310b66dba6b8d8292e49619538662ea38779bcc4
Vinyas N S <vinyas.n.s@gmail.com>
2017-03-28 20:12:45 +0530

    Generics implemented for Heap Sort in Java

--- Java/src/org/openalgorithm/Main.java
+++ Java/src/org/openalgorithm/Main.java
@@ -7,13 +7,13 @@ public class Main { //Testing mergeSort!
         System.out.println("Enter number of items : ");
         int size = new Scanner(System.in).nextInt();
         System.out.println("Enter items : ");
-        double[] arr = new double[size];
+        Double[] arr = new Double[size];
         int i = 0;
         String string = new Scanner(System.in).nextLine();
         for(String s : string.split("\\s+"))    {
             arr[i++] = Double.parseDouble(s);
         }
-        double[] sorted = Sort.heapSort(arr,HeapType.maxHeap);
+        Double[] sorted = Sort.heapSort(arr,HeapType.maxHeap);
         for(double x : sorted)
             System.out.println(x);
 
--- Java/src/org/openalgorithm/Sort.java
+++ Java/src/org/openalgorithm/Sort.java
@@ -1,22 +1,21 @@
 package org.openalgorithm;
 
+import java.util.Arrays;
+
 public final class Sort {
-    public static double[] heapSort(double[] array, HeapType heapType)    {
-        Double[] dArray = new Double[array.length];
-        for(int i = 0;i < array.length;i++)
-            dArray[i] = array[i];
-        BinaryHeap<Double> binaryHeap = new BinaryHeap<>(dArray, heapType);
-        double[] sorted = new double[dArray.length];
+    public static <T extends Comparable<T>> T[] heapSort(T[] array, HeapType heapType)    {
+        T[] dArray = Arrays.copyOf(array,array.length);
+        BinaryHeap<T> binaryHeap = new BinaryHeap<>(dArray, heapType);
         for (int i = 0;i < array.length;i++)  {
             try {
-                sorted[i] = binaryHeap.remove();
+                dArray[i] = binaryHeap.remove();
 
             }
             catch (InvalidOperationException e) {
                 System.out.println(e.getMessage());
             }
         }
-        return sorted;
+        return dArray;
     }
 
     private static double[] merge(double[] a, double[] b) {
@@ -38,15 +37,15 @@ public final class Sort {
         if(array.length == 1)
             return array;
         double[] left = new double[array.length / 2];
-	//for (int i = 0; i < array.length / 2; i++)
-	//	left[i] = array[i];
+        //for (int i = 0; i < array.length / 2; i++)
+        //	left[i] = array[i];
         System.arraycopy(array,0,left,0,array.length / 2);
-	double[] right = new double[array.length - array.length / 2];
-	//for (int i = 0; i < array.length - array.length / 2; i++)
-	//	right[i] = array[array.length / 2 + i];
+        double[] right = new double[array.length - array.length / 2];
+        //for (int i = 0; i < array.length - array.length / 2; i++)
+        //	right[i] = array[array.length / 2 + i];
         System.arraycopy(array,array.length / 2,right,0,array.length - array.length / 2);
-	left = mergeSort(left);
-	right = mergeSort(right);
-	return merge(left, right);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
     }
 }
