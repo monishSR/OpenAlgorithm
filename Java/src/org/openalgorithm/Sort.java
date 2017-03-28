48a1ee89c4cb7fd7f1483288f59e63395702952b
Vinyas N S <vinyas.n.s@gmail.com>
2017-03-28 20:43:45 +0530

    Generics implemented for Merge Sort in Java

--- Java/src/org/openalgorithm/Sort.java
+++ Java/src/org/openalgorithm/Sort.java
@@ -18,11 +18,11 @@ public final class Sort {
         return dArray;
     }
 
-    private static double[] merge(double[] a, double[] b) {
-        double[] merged = new double[a.length + b.length];
+    private static <T extends Comparable<T>> T[] merge(T[] a, T[] b) {
+        T[] merged = Arrays.copyOf(a,a.length+b.length); //Just needed to create T[] array, ignore values in array
         int i = 0, j = 0, k = 0;
         while (i < a.length && j < b.length)
-            if (a[i] <= b[j])
+            if (a[i].compareTo(b[j]) <= 0)
                 merged[k++] = a[i++];
             else
                 merged[k++] = b[j++];
@@ -33,14 +33,14 @@ public final class Sort {
         return merged;
     }
 
-    public static double[] mergeSort(double[] array)  {
+    public static <T extends Comparable<T>> T[] mergeSort(T[] array)  {
         if(array.length == 1)
             return array;
-        double[] left = new double[array.length / 2];
+        T[] left = Arrays.copyOf(array, array.length / 2);
         //for (int i = 0; i < array.length / 2; i++)
         //	left[i] = array[i];
         System.arraycopy(array,0,left,0,array.length / 2);
-        double[] right = new double[array.length - array.length / 2];
+        T[] right = Arrays.copyOf(array,array.length - array.length / 2);
         //for (int i = 0; i < array.length - array.length / 2; i++)
         //	right[i] = array[array.length / 2 + i];
         System.arraycopy(array,array.length / 2,right,0,array.length - array.length / 2);
