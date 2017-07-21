0d8378012aa2115db64929fc13a19e6c24e34e04
Monish S R <srmonish96@gmail.com>
2017-07-21 14:42:52 +0530

    Updated setup.py
    Changed dependencies: seaborn to networkx...And os specific installations

--- setup.py
+++ setup.py
@@ -1,34 +1,39 @@
 import os
 from setuptools import setup
 import sys
+from platform import platform
 
 if sys.version_info < (3, 5):
 	sys.exit('Sorry, Python < 3.5 is not supported')
-
-os.system("sudo apt-get install python3-tk")
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+if 'Ubuntu' in platform():
+	os.system("sudo apt-get install python3-tk")
+elif 'Fedora' in platform():
+	os.system("sudo yum install python3-tk")
+else:
+	sys.stderr.write(
+	"""
+	Error...
+	Different OS...
+	Download and install python3-tk for your specific OS
+	""")
+	sys.exit(0)
 
 setup(
     name = "OpenAnalysis",
     version = "0.0.1",
     author = "OpenWeavers",
-    author_email = "me.vinayakakv@gmail.com",
     description = ("An open source package to analyse and visualise algorithms and data structures"),
     license = "GNU",
     keywords = "OpenWeavers product",
     url = "http://openalgorithm.readthedocs.io",
     packages=['OpenAnalysis'],
-    long_description=read('Readme.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU License",
     ],
     install_requires=[
-	'scipy',
+	'networkx',
 	'numpy',
 	'matplotlib',
     ],
@@ -37,5 +42,5 @@ setup(
             'jupyter', 
             'ipython',
         ],
-    }	
+    },	
 )
